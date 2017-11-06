3b731175d90b07fc9abc277617a5bfa8b5c8d2cb
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-06 15:23:38 +0100

    refs #5: Added failing test for creating an author.

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  
+  it "should create a valid instance when a new author is created" do
+    first_name = 'Alan'
+    last_name = 'Turing'
+    homepage = 'www.wikipedia.org/Alan_Turing'
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+
+    expect(author.first_name).to eq(first_name)
+    expect(author.last_name).to eq(last_name)
+    expect(author.homepage).to eq(homepage)
+  end
+
+  
+end
