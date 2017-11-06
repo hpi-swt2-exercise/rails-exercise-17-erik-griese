e0af573aedcd8afeddfea2964d96299c5ede15d3
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-06 22:05:44 +0100

    refs #15: Made test more strict again. Changed case to 'Add author'

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -19,7 +19,7 @@ describe "Author index page", type: :feature do
   it "should have a link to the new author page" do
     visit authors_path
 
-    expect(page).to have_link("add author")
+    expect(page).to have_link("Add author", href: new_author_path)
   end
 
 end
