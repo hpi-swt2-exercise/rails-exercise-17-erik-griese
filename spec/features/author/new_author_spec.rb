d260720d816da611f95f289b23236fa4f030302a
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-06 16:18:13 +0100

    refs #7: Added failing test for successfully saving new author

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -19,4 +19,13 @@ describe "New author page", type: :feature do
 
     expect(page).to have_css('input[type="submit"]')
   end
+
+  it "should be possible to save a new author without errors" do
+    visit new_author_path
+
+    fill_in("author_first_name", with: "Alan")
+    fill_in("author_last_name", with: "Turing")
+    fill_in("author_homepage", with: "www.wikipedia.org/Alan_Turing")
+    find('input[type="submit"]').click
+  end
 end
