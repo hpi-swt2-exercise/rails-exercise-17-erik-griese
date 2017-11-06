8374401350c17b2d054f0e17be877ad269b2476f
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-06 16:48:21 +0100

    refs #8: Correctly saving newly created authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,5 +4,12 @@ class AuthorsController < ApplicationController
 
   def create
     render plain: params[:author].inspect
+    @author = Author.new(author_params)
+    @author.save
   end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
