5700d8f3f1562c8a5c2465a19893adc3695e4afc
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-07 19:07:07 +0100

    refs #20: Added feature test, whether updated author information is saved

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -10,4 +10,25 @@ describe "Edit author page", type: :feature do
     author.destroy
   end
 
+  it "should save changes to the author" do
+    author = FactoryGirl.create :author
+
+    updated_first_name = "Bob"
+    updated_last_name ="Turner"
+    updated_homepage = "www.bobby.de"
+
+    visit edit_author_path(author)
+    fill_in("author_first_name", with: updated_first_name)
+    fill_in("author_last_name", with: updated_last_name)
+    fill_in("author_homepage", with: updated_homepage)
+    find('input[type="submit"]').click
+
+    updated_author = Author.find(author.id)
+    expect(updated_author.first_name).to eq(updated_first_name)
+    expect(updated_author.last_name).to eq(updated_last_name)
+    expect(updated_author.homepage).to eq(updated_homepage)
+
+    updated_author.destroy
+  end
+
 end
