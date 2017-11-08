bd166b908231cc67b60aebd23c3de67400851ed7
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-08 15:23:15 +0100

    refs #27: Checking correct validation for integer paper years

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -20,4 +20,13 @@ RSpec.describe Paper, type: :model do
     expect(paper).not_to be_valid
   end
 
+  it "should fail validation with non-integer year" do
+    paper_authors = FactoryGirl.build_list :author, 1
+    paper = Paper.new(title: "Breaking Time", venue: "Mgz", year: "Sexteen", authors: paper_authors)
+    expect(paper).not_to be_valid
+
+    paper_float = Paper.new(title: "Breaking Time", venue: "Mgz", year: "2015.75", authors: paper_authors)
+    expect(paper_float).not_to be_valid
+  end
+
 end
