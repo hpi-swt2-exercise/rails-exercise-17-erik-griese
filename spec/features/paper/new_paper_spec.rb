6b75bc6120253fd23de75b54c8ea795a3bd1a609
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-08 14:36:26 +0100

    refs #23: Added first tests for paper model. Testing whether new paper page has form fields and submit button

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,5 +5,15 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_paper_path
   end
+
+  it "should have fields for title, venue, year and a submit button" do
+    visit new_paper_path
+
+    expect(page).to have_field("Title")
+    expect(page).to have_field("Venue")
+    expect(page).to have_field("Year")
+
+    expect(page).to have_css('input[type="submit"]')
+  end
   
 end
