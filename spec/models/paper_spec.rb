90fa39c8336b83043192a08e12a30d22f0fc4271
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-08 14:47:59 +0100

    refs #24: testing correct validation of paper title

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
+  it "should fail validation without a title" do
+    paper_authors = FactoryGirl.build_list :author, 1
+    paper = Paper.new(venue: "Mgz", year: 2017, authors: paper_authors)
+    expect(paper).not_to be_valid
+  end
 end
