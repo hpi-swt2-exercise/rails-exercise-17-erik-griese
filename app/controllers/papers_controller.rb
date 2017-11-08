20366e9433d58025956a7f7e2098a90c3e81e550
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-08 16:11:24 +0100

    refs #28: Saving author in the new paper page

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,5 +1,35 @@
 class PapersController < ApplicationController
+
+  def show
+    @paper = Paper.find(params[:id])
+  end
+  
   def new
     @paper = Paper.new
   end
+
+  def create
+    params_safe = paper_params
+    if params_safe.has_key?(:year)
+      try_convert_year(params_safe)
+    end
+    @paper = Paper.new(params_safe)
+
+    if @paper.save
+      redirect_to @paper
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def paper_params
+      params.require(:paper).permit(:title, :venue, :year)
+    end
+
+    def try_convert_year(params_safe)
+      num_string = params_safe[:year]
+      num = num_string.to_i
+      params_safe[:year] = if num.to_s == num_string then num else num_string end
+    end
 end
