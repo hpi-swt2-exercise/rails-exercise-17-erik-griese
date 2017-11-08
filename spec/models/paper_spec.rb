09bd701bab3a9762b442b7cd722da316080d1c3d
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-08 14:58:17 +0100

    refs #25: Testing validation of paper venue

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
+
   it "should fail validation without a title" do
     paper_authors = FactoryGirl.build_list :author, 1
     paper = Paper.new(venue: "Mgz", year: 2017, authors: paper_authors)
     expect(paper).not_to be_valid
   end
+
+  it "should fail validation without a venue" do
+    paper_authors = FactoryGirl.build_list :author, 1
+    paper = Paper.new(title: "Breaking Time", year: 2017, authors: paper_authors)
+    expect(paper).not_to be_valid
+  end
+
 end
