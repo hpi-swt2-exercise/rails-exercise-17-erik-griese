3ab3bdd9be7b46ab780645bb7c7309ec1a026178
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2017-11-09 14:44:16 +0100

    refs #33: Test whether edit paper page renders and saves changes

---
+++ spec/features/paper/edit_paper_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "Edit paper page", type: :feature do
+
+  it "should render" do
+    paper = FactoryGirl.create :paper
+
+    visit edit_paper_path(paper)
+
+    paper.destroy
+  end
+
+  it "should save changes" do
+    paper = FactoryGirl.create :paper
+
+    visit edit_paper_path(paper)
+
+    title = "Updated Title"
+    venue = "Updated Venue"
+    year = "2020"
+
+    fill_in("Title", with: title)
+    fill_in("Venue", with: venue)
+    fill_in("Year", with: year)
+
+    find('input[type="submit"]').click
+
+    updated_paper = Paper.find(paper.id)
+
+    expect(updated_paper.title).to eq(title)
+    expect(updated_paper.venue).to eq(venue)
+    expect(updated_paper.year.to_s).to eq(year)
+
+    updated_paper.destroy
+  end
+
+end
